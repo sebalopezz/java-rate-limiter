e6c7b6ddbd37316ec1a7e8b0045062c5b1d80db3
Jorge Sebastian Lopez <jorgesebastianlopez@gmail.com>
2022-01-22 19:37:30 -0300

    Handle empty or null header for userID.

--- src/main/java/com/example/demo/interceptors/RateLimiterInterceptor.java
+++ src/main/java/com/example/demo/interceptors/RateLimiterInterceptor.java
@@ -2,6 +2,7 @@ package com.example.demo.interceptors;
 
 import com.example.demo.cache.RateLimiterCacheClient;
 import com.example.demo.utils.RateLimiter;
+import com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -21,15 +22,24 @@ public class RateLimiterInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String userId = request.getHeader(HEADER_USER_ID);
+
+        if (Strings.isNullOrEmpty(userId)) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            return false;
+        }
+
         RateLimiter rateLimiter = rateLimiterCacheClient.get(userId);
+
         if (rateLimiter == null) {
             rateLimiter = new RateLimiter();
         }
+
         if (rateLimiter.allowRequest()) {
             rateLimiterCacheClient.put(userId, rateLimiter);
             response.setStatus(HttpStatus.OK.value());
             return true;
         }
+
         response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
         return false;
     }
